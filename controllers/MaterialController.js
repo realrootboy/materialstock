1b8838942d9f5753d3d23c69cecdd48e6d276533
Renan M. Gomes <renangomes.es@gmail.com>
2021-09-26 18:42:53 -0300

    Controller + requisiçoes do model Material

--- controllers/MaterialController.js
+++ controllers/MaterialController.js
@@ -1,6 +1,8 @@
 const {sequelize, Material} = require("./../models");
 const router = require("express").Router();
 
+const basePath = "/material";
+
 class MaterialController {
     
     static get = async (req, res) => {
@@ -9,10 +11,53 @@ class MaterialController {
         return res.status(200).json(materials);
     }
 
+    static post = async (req, res) => {
+        const { name } = req.body;
+
+        const material = await Material.create({name});
+
+        return res.status(200).json(material);
+    }
+
+    static put = async (req, res) => {
+        const { id } = req.params;
+        const { name } = req.body;
+
+        const material = await Material.findOne({
+            where: {
+                id
+            }
+        });
+
+        if(!material) res.status(401).json({id, message: "ID not found"});
+
+        const update = await material.update({name});
+
+        return res.status(200).json(update);
+    }
+
+    static delete = async (req, res) => {
+        const { id } = req.params;
+
+        const material = await Material.findOne({
+            where: { id }
+        });
+
+        if(!material) res.status(401).json({id, message: "ID not found"});
+
+        const destroy = await material.destroy();
+
+        return res.status(200).json(destroy);
+
+    }
 }
 
 module.exports = (()=>{
     
-    router.get("/material", MaterialController.get);
+    router.get(basePath, MaterialController.get);
+    router.post(basePath, MaterialController.post);
+    router.put(`${basePath}/:id`, MaterialController.put);
+    router.delete(`${basePath}/:id`, MaterialController.delete);
+
     return router;
 })();
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { sequelize } = require("./models");
 const app = (new Server()).app;
 
 (async () => {
-    await sequelize.sync({ force: true });
+    await sequelize.sync({ alter: true, logging: false });
 
     app.listen(8080, () => {
         console.log("App listening in 8080");
