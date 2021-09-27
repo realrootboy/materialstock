c7229c6da854ab0d1b0ec9743634f091ab16b302
Renan M. Gomes <renangomes.es@gmail.com>
2021-09-27 19:56:09 -0300

    Mais alguns models e uns relacionamentos

--- models/lease.js
+++ models/lease.js
@@ -4,13 +4,14 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Lease extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      models.Lease.belongsToMany(models.Employee, {
+        through: 'LeaseEmployees',
+        as: 'employees',
+        foreignKey: 'LeaseId',
+        onDelete: 'cascade'
+      });
+      models.Lease.belongsTo(models.Costumer);
     }
   };
   Lease.init({
