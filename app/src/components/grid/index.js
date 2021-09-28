d8c4d84ea3dadf71c387ae0f3488bb6f0d307b2d
icaro156 <icaro1561@outlook.com>
2021-09-28 17:46:05 -0300

    working in the item list

---
+++ app/src/components/grid/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Grid } from '@mui/material';
+function List() {
+  return(
+    <Grid >
+      <Grid item lg={3} md={4} sm={6} xs={12}>
+        teste
+      </Grid>
+      <Grid item lg={3} md={4} sm={6} xs={12}>
+        teste
+      </Grid>
+      <Grid item lg={3} md={4} sm={6} xs={12}>
+        teste
+      </Grid>
+      <Grid item lg={3} md={4} sm={6} xs={12}>
+        teste
+      </Grid>
+      <Grid item lg={3} md={4} sm={6} xs={12}>
+        teste
+      </Grid>
+      <Grid item lg={3} md={4} sm={6} xs={12}>
+        teste
+      </Grid>
+    </Grid>
+  )
+}
+
+export default List
--- app/src/pages/materialRegister/index.js
+++ app/src/pages/materialRegister/index.js
@@ -1,69 +1,23 @@
-import React, {useState} from 'react';
-import Topbar from '../../components/topbar/';
-import Button from '@material-ui/core/Button';
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import TextField from '@material-ui/core/TextField';
-import { Typography } from '@mui/material';
+import React from 'react';
 
-import api from '../../services/api';
+import Topbar from '../../components/topbar/';
+import Drawer from '../../components/drawer';
+import MaterialForm from '../../components/materialForm'
+import List from '../../components/grid'
+import Container from '@material-ui/core/Container'
+
 
 const MaterialRegister = () => {
-  const[name, setName]= useState('');
-  const[description, setDescription]= useState('');
-  
-  async function handleNewItem(e){
-    e.preventDefault();
-    try{
-      await api.post('/material', {
-        "name": name,
-        "descricao": description,
-      })
-    }
-    catch(err){
 
-    }
-  }
   
 
   return (
-    <container fixed>
+    <Container>
       <Topbar/>
-      <Container sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-        <form onSubmit={handleNewItem}>
-          <Grid container spacing={3}>
-            <Grid item xs={12}>
-              <Grid container spacing={2}>
-                <Grid item xs={12}>
-                <Typography variant="h4" component="h2" mt={5} align='center'>Cadastro de itens</Typography>
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField fullWidth label="Nome" name="nome" size="small" variant="outlined" value={name} onChange={e => setName(e.target.value)}/>
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    fullWidth
-                    label="Descrição"
-                    name="descricao"
-                    size="big"
-                    multiline
-                    type="text"
-                    variant="outlined"
-                    value={description} 
-                    onChange={e => setDescription(e.target.value)}
-                  />
-                </Grid>
-              </Grid>
-            </Grid>
-            <Grid item xs={12}>
-              <Button color="secondary" fullWidth type="submit" variant="contained">
-                Cadastrar material
-              </Button>
-            </Grid>
-          </Grid>
-        </form>
-      </Container>
-    </container>
+      <Drawer/>
+      <MaterialForm/>
+      <List/>  
+    </Container>
   );
 }
 
