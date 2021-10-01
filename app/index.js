af7b2e8aff45f7530ee6ecc701d521469ce592c2
icaro156 <icaro1561@outlook.com>
2021-10-01 14:45:48 -0300

    fix: electron bullet

--- app/index.js
+++ app/index.js
@@ -1,14 +1,24 @@
-const { app, BrowserWindow } =require('electron')
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
 function createWindow () {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
   })
 
-  win.loadURL("http://localhost:3000")
+  win.loadFile(path.join('build','index.html'))
 }
 
-app.whenReady().then(() => {
-  createWindow()
+app.on('ready',createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
 })
