71fa229a36403714ebe38e56073d80558b737661
Renan M. Gomes <renangomes.es@gmail.com>
2021-10-01 16:48:35 -0300

    fix size

--- app/index.js
+++ app/index.js
@@ -3,8 +3,8 @@ const path = require('path')
 
 function createWindow () {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1440,
+    height: 1024,
   })
 
   win.loadFile(path.join('build','index.html'))
